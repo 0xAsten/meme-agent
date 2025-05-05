c056286e5c5ae5392b687a3595aae59b878a9298
Asten <0xasten@gmail.com>
2025-05-05 21:10:16 +0800

    update subgraph

--- graph/src/meme-nft.ts
+++ graph/src/meme-nft.ts
@@ -1,105 +1,32 @@
+import { Bytes } from '@graphprotocol/graph-ts'
 import {
-  Approval as ApprovalEvent,
-  ApprovalForAll as ApprovalForAllEvent,
-  BatchMetadataUpdate as BatchMetadataUpdateEvent,
   MemeMinted as MemeMintedEvent,
-  MetadataUpdate as MetadataUpdateEvent,
-  Transfer as TransferEvent
-} from "../generated/MemeNFT/MemeNFT"
-import {
-  Approval,
-  ApprovalForAll,
-  BatchMetadataUpdate,
-  MemeMinted,
-  MetadataUpdate,
-  Transfer
-} from "../generated/schema"
-
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-  entity.tokenId = event.params.tokenId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-  let entity = new ApprovalForAll(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleBatchMetadataUpdate(
-  event: BatchMetadataUpdateEvent
-): void {
-  let entity = new BatchMetadataUpdate(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._fromTokenId = event.params._fromTokenId
-  entity._toTokenId = event.params._toTokenId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+  Transfer as TransferEvent,
+} from '../generated/MemeNFT/MemeNFT'
+import { MemeNFT } from '../generated/schema'
 
 export function handleMemeMinted(event: MemeMintedEvent): void {
-  let entity = new MemeMinted(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
+  let entity = new MemeNFT(
+    Bytes.fromHexString(event.params.tokenId.toHexString()),
   )
+
   entity.tokenId = event.params.tokenId
   entity.owner = event.params.owner
   entity.tokenURI = event.params.tokenURI
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleMetadataUpdate(event: MetadataUpdateEvent): void {
-  let entity = new MetadataUpdate(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._tokenId = event.params._tokenId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
 
   entity.save()
 }
 
 export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
+  let entity = MemeNFT.load(
+    Bytes.fromHexString(event.params.tokenId.toHexString()),
   )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  if (!entity) {
+    return
+  }
+
+  entity.owner = event.params.to
 
   entity.save()
 }
